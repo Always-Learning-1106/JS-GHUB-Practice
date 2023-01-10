205365957dd6dd6932cffd7c49107e4a37f9b844
Glenn Eddins <1106glenn@gmail.com>
2023-01-10 16:27:35 -0500

    working more on switch

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function trying(a) {
-  let b = a + 2;
-  return b;
-} ///ALWAYS return something from a function otherwise get undefined behavior
-let testing = trying(-1);
+// function trying(a) {
+//   let b = a + 2;
+//   return b;
+// } ///ALWAYS return something from a function otherwise get undefined behavior
+// let testing = trying(-1);
 
-switch (testing) {
-  case 1:
-    console.log(`trying = -1`);
-    break;
-  case 2:
-    console.log(`trying = 0`);
-    break;
-  case 3:
-    console.log(`trying = 1`);
-    break;
-  default:
-    console.log(`I don't know anything`);
-}
+// switch (testing) {
+//   case 1:
+//     console.log(`trying = -1`);
+//     break;
+//   case 2:
+//     console.log(`trying = 0`);
+//     break;
+//   case 3:
+//     console.log(`trying = 1`);
+//     break;
+//   default:
+//     console.log(`I don't know anything`);
+// }
 //  switch(the case answer goes here){
 // case possible answer:
 //   do something
@@ -25,3 +25,16 @@ switch (testing) {
 
 //dont forget to add default
 // }
+
+let converter = function convert(x) {
+  switch (typeof x) {
+    case "number":
+      return `${x.toString()} was a number, but now it is a string`;
+    case "string":
+      return `"${x} is a string"`;
+
+    default:
+      return `x has a boolean value of ${!!x}`;
+  }
+};
+console.log(converter("false"));
