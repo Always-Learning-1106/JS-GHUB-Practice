8c7b2b951dd510facfb1d555012ffa42000150f4
Glenn <1106glenn@gmail.com>
2023-01-21 23:42:19 -0500

    challenge using arrays pretty easy

--- script.js
+++ script.js
@@ -1434,11 +1434,11 @@ GOOD LUCK 😀
 // f();
 // h();
 // f();
-const currencies = new Map([
-  ["USD", "United States Dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound Sterling"],
-]);
+// const currencies = new Map([
+//   ["USD", "United States Dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound Sterling"],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // const arr = ["a", "b", "c", "d", "e"];
@@ -1467,15 +1467,58 @@ const currencies = new Map([
 // console.log(arr["3"]);
 
 // console.log(arr);
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const forEach = movements.forEach((move, i) =>
-  move <= 0
-    ? console.log(`movement #${i + 1}: withdraw of $${move}`)
-    : console.log(`movement #${i + 1}: deposit of $${move}`)
-);
-console.log(`**********for of compared to forEach above`);
-for (const [i, transaction] of movements.entries()) {
-  transaction <= 0
-    ? console.log(`movement #${i + 1}: withdraw of $${transaction}`)
-    : console.log(`movement #${i + 1}: deposit of $${transaction}`);
-}
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const forEach = movements.forEach((move, i) =>
+//   move <= 0
+//     ? console.log(`movement #${i + 1}: withdraw of $${move}`)
+//     : console.log(`movement #${i + 1}: deposit of $${move}`)
+// );
+// console.log(`**********for of compared to forEach above`);
+// for (const [i, transaction] of movements.entries()) {
+//   transaction <= 0
+//     ? console.log(`movement #${i + 1}: withdraw of $${transaction}`)
+//     : console.log(`movement #${i + 1}: deposit of $${transaction}`);
+// }
+// const currencies = new Map([
+//   ["USD", "United States Dollar"],
+//   ["EUR", "Euro"],
+//   ["GBP", "Pound Sterling"],
+// ]);
+
+// const currMap = currencies.forEach((value, key, map) => {
+//   console.log(`${value}: ${key}`);
+// });
+// const currSet = new Set(["USD", "GBP", "EUR"]);
+// const setCurr = currSet.forEach((value, _, set) => {
+//   console.log(`${value}: ${value}`);
+// });
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+HINT: Use tools from all lectures in this section so far 😉
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+GOOD LUCK 😀
+*/
+const julia = [3, 5, 2, 12, 7];
+const juliaDogs = julia.slice(1, 3);
+console.log(juliaDogs);
+const kate = [4, 1, 15, 8, 3];
+const concatDogs = juliaDogs.concat(kate);
+
+// const concatDogs = [...juliaDogs,...kate]
+const checkDogs = function (concatDogs) {
+  concatDogs.forEach((dog, i) => {
+    dog <= 3
+      ? console.log(`Dog ${i + 1} is a puppy and ${dog} years old`)
+      : console.log(`Dog ${i + 1} is an adult and is ${dog} years old`);
+  });
+};
+checkDogs(concatDogs);
